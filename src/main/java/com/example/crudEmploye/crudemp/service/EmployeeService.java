32d80f23202ced758ed4969f7fcf57f08497490d
BenAhmedAmir <dragotn@gmail.com>
2021-01-14 21:38:03 +0100

    springboot thymeleaf crud db

---
+++ src/main/java/com/example/crudEmploye/crudemp/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.crudEmploye.crudemp.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.crudEmploye.crudemp.model.Employee;
+
+public interface EmployeeService {
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	Page<Employee> findPaginated(int pageNo, int pageSize);
+	Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+
+
+}
